12254fd76affc06ec10e829511f4116f734ce57c
daoliwoniu <1446854445@qq.com>
2019-11-11 08:56:20 +0800

    1

---
+++ src/components/Coupon/index.tsx
@@ -0,0 +1,16 @@
+import React, { memo } from "react"
+// import styles from './index.less'
+
+export type ModalProps = {
+  text: string
+  color: string
+  fontSize: number
+  children?: any
+}
+
+export default memo((props: ModalProps) => {
+
+  return <>
+
+  </>
+})
